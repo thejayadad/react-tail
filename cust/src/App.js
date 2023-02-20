93599b7ae66884b077fc63dfad4ec1ba09abea4d
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-02-20 15:26:58 -0500

    update

---
+++ cust/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css"
+import ListHeader from './components/ListHeader'
+
+const App = () => {
+  return (
+    <div className='box'>
+      <ListHeader />
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,35 @@
+const PORT = process.env.PORT ?? 8000
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require('dotenv').config();
+const cors = require("cors");
+const authRoutes = require("./routes/auth.js");
+
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/auth", authRoutes);
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.set('strictQuery', true);
+mongoose.connect(uri, { useNewUrlParser: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB Connected");
+})
+
+app.get("/notes", async(req, res) => {
+    try {
+
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+
+
+app.listen(PORT, () => console.log(`Listening ${PORT}`))
