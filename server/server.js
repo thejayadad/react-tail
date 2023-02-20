4ce3ec4f4a806ed40a90a606d3154f26346568c9
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-02-20 15:34:15 -0500

    udpate

--- server/server.js
+++ server/server.js
@@ -5,12 +5,18 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 const cors = require("cors");
 const authRoutes = require("./routes/auth.js");
+const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
 
 
 app.use(express.json());
 app.use(cors());
 app.use("/api/auth", authRoutes);
+app.use(
+    "/api/users/:id/messages",
+    loginRequired,
+    ensureCorrectUser,
 
+  );
 
 const uri = process.env.ATLAS_URI;
 mongoose.set('strictQuery', true);
